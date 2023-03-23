67b525aea538925a7687779809721e08f1616953
Peter Olah <peter.olah@bestow.com>
2023-03-22 21:43:34 -0500

    Modal is now working

--- src/DetailsBrian.jsx
+++ src/DetailsBrian.jsx
@@ -1,15 +1,14 @@
-import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import Modal from "./Modal";
+import ErrorBoundary from "./ErrorBoundary";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
-import ErrorBoundary from "./ErrorBoundary";
-import Modal from "./Modal";
 
 const Details = () => {
-  const [showModal, setShowModal] = useState(false);
   const { id } = useParams();
-
+  const [showModal, setShowModal] = useState(false);
   const results = useQuery(["details", id], fetchPet);
 
   if (results.isLoading) {
@@ -27,9 +26,7 @@ const Details = () => {
       <Carousel images={pet.images} />
       <div>
         <h1>{pet.name}</h1>
-        <h2>
-          {pet.animail} - {pet.breed} - {pet.city} - {pet.state}
-        </h2>
+        <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
         <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
         <p>{pet.description}</p>
         {showModal ? (
@@ -48,12 +45,10 @@ const Details = () => {
   );
 };
 
-function DetailsErrorBoundary() {
+export default function DetailsErrorBoundary(props) {
   return (
     <ErrorBoundary>
-      <Details></Details>
+      <Details {...props} />
     </ErrorBoundary>
   );
 }
-
-export default DetailsErrorBoundary;
